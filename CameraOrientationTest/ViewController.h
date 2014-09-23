6e9e25943931690e2390d3e947217858ed84bca8
Jason Terhorst <jason.terhorst@precisionplanting.com>
2014-09-23 11:03:51 -0500

    Setup

--- CameraOrientationTest/ViewController.h
+++ CameraOrientationTest/ViewController.h
@@ -8,8 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
+-(IBAction)openCamera:(id)sender;
 
 @end
 
--- CameraOrientationTest/ViewController.m
+++ CameraOrientationTest/ViewController.m
@@ -16,12 +16,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+-(IBAction)openCamera:(id)sender {
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        UIImagePickerController* picker = [[UIImagePickerController alloc] init];
+        picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        picker.delegate = self;
+        picker.showsCameraControls = YES;
+        [self presentViewController:picker animated:NO completion:nil];
+    }
 }
 
 @end
